aaac36e4432f6d2114d063e4072debec7d974951
sushanthj674 <153906968+sushanthj674@users.noreply.github.com>
2024-12-18 18:55:48 +0530

    sorted strings

--- sort/numberSort.js
+++ sort/numberSort.js
@@ -1,4 +1,4 @@
-let numbers = [1, 2, 1, 3, 1, 1, 2, 4, 5];
+let numbers = [1,3,2,4,5,8,7];
 
 const ascending = function (a, b) {
   if (a === b) {
---
+++ sort/stringsSorting.js
@@ -0,0 +1,5 @@
+const fruits = 'watermelon,banana,capple,papple,papaya,apple'.split(',');
+
+fruits.sort();
+
+console.log(fruits);
